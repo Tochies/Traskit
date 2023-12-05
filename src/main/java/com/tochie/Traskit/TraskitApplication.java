c764c2deb63542e2d8b77be83d8a5e3f2ac7d405
Tochi Emeasoba <tochiswork@gmail.com>
2023-12-05 10:30:02 +0000

    traskit initial bootstrap project

---
+++ src/main/java/com/tochie/Traskit/TraskitApplication.java
@@ -0,0 +1,13 @@
+package com.tochie.Traskit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraskitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraskitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tochie/Traskit/TraskitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tochie.Traskit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraskitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
