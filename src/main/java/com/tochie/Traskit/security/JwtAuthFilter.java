3f443e4f1ab7e1a2281b7b35be96ada5de16c7aa
Tochi Emeasoba <tochiswork@gmail.com>
2023-12-30 08:15:12 +0000

    implement task creation

--- src/main/java/com/tochie/Traskit/security/JwtAuthFilter.java
+++ src/main/java/com/tochie/Traskit/security/JwtAuthFilter.java
@@ -1,9 +1,6 @@
 package com.tochie.Traskit.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tochie.Traskit.enums.ResponseCodeEnum;
-import com.tochie.Traskit.exception.ErrorResponse;
-import com.tochie.Traskit.exception.GenericException;
+import com.tochie.Traskit.exception.AuthException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,50 +9,43 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @Slf4j
-@Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtService jwtService;
 
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    @Autowired
+    public JwtAuthFilter(HandlerExceptionResolver handlerExceptionResolver){
+        this.handlerExceptionResolver = handlerExceptionResolver;
+    }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, GenericException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, AuthException {
 
-        String token = jwtService.resolveToken(request);
+        try {
+            String token = jwtService.resolveToken(request);
 
-        if (token != null && jwtService.validateToken(token)) {
-            Authentication auth = jwtService.getAuthentication(response, request);
-            SecurityContextHolder.getContext().setAuthentication(auth);
+            if (token != null && jwtService.validateToken(token)) {
+                Authentication auth = jwtService.getAuthentication(response, request);
+                SecurityContextHolder.getContext().setAuthentication(auth);
 
-        } else {
+            }
 
+            filterChain.doFilter(request, response);
+        } catch (AuthException e){
             SecurityContextHolder.clearContext();
-
+            handlerExceptionResolver.resolveException(request, response, null, e);
         }
 
-
-        filterChain.doFilter(request, response);
     }
 
-    private void handleJWTException(HttpServletResponse response) throws IOException {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType("application/json");
-
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setCode(ResponseCodeEnum.FAILED_AUTHENTICATION.getCode());
-        errorResponse.setDescription("Failed authentication");
-
-        PrintWriter writer = response.getWriter();
-        new ObjectMapper().writeValue(writer, errorResponse);
-        writer.flush();
-    }
 
 }
