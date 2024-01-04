ddb11bb2e540f240dcff307cdd221764c3f6c8f8
Tochi Emeasoba <tochiswork@gmail.com>
2024-01-04 00:14:46 +0000

    add edit task api

--- src/main/java/com/tochie/Traskit/service/TaskService.java
+++ src/main/java/com/tochie/Traskit/service/TaskService.java
@@ -19,13 +19,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
 @Slf4j
 @Service
-
 public class TaskService {
 
 
